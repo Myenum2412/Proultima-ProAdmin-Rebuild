3d3f2df514d3614958daa554bf36d0182528768b
Najas06 <najasneju@gmail.com>
2026-06-20 15:21:26 +0530

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { GalleryVerticalEndIcon } from "lucide-react";
+import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEndIcon className="size-4" />
+            </div>
+            Acme Inc.
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <Image
+          width={720}
+          height={720}
+          src="https://blog.woodenstreet.com/images/data/image_upload/16613308681650972289office-design-ideas-trends-banner-min.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
